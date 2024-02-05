d70017fb0ddb6e8c65617bf9ab1efdb32b8bd455
Feng Shu <tumashu@163.com>
2024-02-05 18:44:53 +0800

    u

--- themes/resize-theme.sh
+++ themes/resize-theme.sh
@@ -1,3 +1,3 @@
 cd GeeIcewm
 cp *.xpm ../GeeIcewm-Big
-for f in title*.xpm menu*.xpm max*.xpm min*.xpm restore*.xpm close*.xpm; do convert $f -resize 130% ../GeeIcewm-Big/$f; done
+for f in title*.xpm menu*.xpm max*.xpm min*.xpm restore*.xpm close*.xpm; do convert $f -resize 150% ../GeeIcewm-Big/$f; done
